4caca359ff51548c916634ad265a8a5c32acb3f1
Shella-translator <122383414+Shella-translator@users.noreply.github.com>
2023-01-27 23:33:22 +0300

    Add files via upload
    Отправила файлы

---
+++ Задача2.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+int max = 0;
+int min = 0;
+
+Console.Write("Введите первое число: ");
+int first = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int second = Convert.ToInt32(Console.ReadLine());
+
+if(first > second)
+{
+    max = first;
+    min = second;
+}
+else
+{
+    max = second;
+    min = first;
+}
+Console.WriteLine("max = " + max + " и " + "min = " + min);
---
+++ Задача4.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int l = Convert.ToInt32(Console.ReadLine());
+int max = n;
+if (max < m)
+    max = m;
+if (max < l) 
+    max = l;
+Console.WriteLine($"{max} максимальное из трех чисел");
