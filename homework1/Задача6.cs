8740f697e0e7476295c9aa3faae550d580d99758
Shella-translator <122383414+Shella-translator@users.noreply.github.com>
2023-01-28 09:30:07 +0300

    Add files via upload
    Files uploaded

---
+++ homework1/Задача6.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0)
+    Console.Write("Четное");
+else Console.WriteLine("Нечетное");
