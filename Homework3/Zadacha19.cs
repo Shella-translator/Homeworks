021f91cf5dba57d506346279c92a46022baa9e87
Shella-translator <122383414+Shella-translator@users.noreply.github.com>
2023-02-04 18:51:32 +0300

    Add files via upload
    Добавлены программы

---
+++ Homework3/Zadacha19.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 10000 || n > 99999)
+{
+    Console.Write($"{n} не является пятизначным");
+    return;
+}
+int n1 = n / 10000;
+int n2 = n / 1000 % 10;
+int n4 = n / 10 % 10;
+int n5 = n % 10;
+if (n1 == n5 && n2 == n4)
+{
+    Console.WriteLine ($"{n} является палиндромом");
+}
+else
+{
+    Console.WriteLine ($"{n} не является палиндромом");
+}
