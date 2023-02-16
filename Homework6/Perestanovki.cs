e6af8c69103edc1f60af9ac8aece925b3b56bebc
Shella-translator <122383414+Shella-translator@users.noreply.github.com>
2023-02-16 09:39:33 +0300

    Add files via upload

---
+++ Homework6/Perestanovki.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        char[] chs = s.ToCharArray();
+
+        Perm(chs, 0);
+    }
+
+    public static void Perm(char[] chs, int i)
+    {
+        if (i == chs.Length)
+        {
+            Console.WriteLine(chs);
+            return;
+        }
+
+        for (int j = i; j < chs.Length; j++)
+        {
+            Swap(ref chs[i], ref chs[j]);
+            Perm(chs, i + 1);
+            Swap(ref chs[i], ref chs[j]);
+        }
+    }
+
+    public static void Swap(ref char a, ref char b)
+    {
+        char temp = a;
+        a = b;
+        b = temp;
+    }
+}
---
+++ Homework6/Trianglearea.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        var line = Console.ReadLine().Split(' ');
+        var x1 = int.Parse(line[0]);
+        var y1 = int.Parse(line[1]);
+        var x2 = int.Parse(line[2]);
+        var y2 = int.Parse(line[3]);
+        var x3 = int.Parse(line[4]);
+        var y3 = int.Parse(line[5]);
+
+        var a = Math.Sqrt(Math.Abs(x2 - x1) * Math.Abs(x2 - x1) + Math.Abs(y2 - y1) * Math.Abs(y2 - y1));
+        var b = Math.Sqrt(Math.Abs(x2 - x3) * Math.Abs(x2 - x3) + Math.Abs(y2 - y3) * Math.Abs(y2 - y3));
+        var c = Math.Sqrt(Math.Abs(x1 - x3) * Math.Abs(x1 - x3) + Math.Abs(y1 - y3) * Math.Abs(y1 - y3));
+
+        var p = (a + b + c) / 2;
+        var s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+        if (Math.Abs(s - (int)s) < 0.00001)
+        {
+            Console.WriteLine((int)s);
+        }
+        else
+        {
+            Console.WriteLine(Math.Round(s, 1));
+        }
+    }
+}
