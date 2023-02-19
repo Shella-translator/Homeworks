b1f3ac93cac618d37c5b4f216c85fb9e3ec676e3
Shella-translator <122383414+Shella-translator@users.noreply.github.com>
2023-02-19 10:13:49 +0300

    Add files via upload

---
+++ Homework7/MishaNegativ.cs
@@ -0,0 +1,47 @@
+﻿void InputPrintPhoto(string[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i % 2 == 0 && j == 0 || i % 2 == 0 && j == 3 ) matrix[i, j] = "W";
+            else matrix[i, j] = "B";
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+void MakeNegativ(string[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i % 2 == 0 && j == 0 || i >= 1 && j == 3) matrix[i, j] = "B";
+            else matrix[i, j] = "W";
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int ChekNegotive(string[,] matrix1, string[,] matrix2)
+{
+    int fault = 0;
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix1.GetLength(1); j++)
+        {
+            if( matrix1[i, j] == matrix2[i, j]) fault++;
+        }
+     }
+     return fault;
+}
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();  
+string[,] Photo = new string[size[0], size[1]];
+string[,] Negativ = new string[size[0], size[1]];
+InputPrintPhoto(Photo);
+Console.WriteLine();
+MakeNegativ(Negativ);
+Console.WriteLine($"Количество ошибок : {ChekNegotive(Photo, Negativ)}");
---
+++ Homework7/ZapolnenieDiagonalymi.cs
@@ -0,0 +1,32 @@
+﻿void DiagInp(int[,] matrix)
+{
+    int i, j, row = 0, col = 0, n = 0;
+    while (row <= matrix.GetLength(0) - 1)
+    {
+        i = row;
+        j = col;
+        while (i < matrix.GetLength(0) && j >= 0)
+        {
+            matrix[i++, j--] = n++;
+        }
+        if( col < matrix.GetLength(1) - 1) col++;
+        else row++;
+    }
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите размеры массива через пробел: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+int r = size[0] * size[1];
+DiagInp(matrix);
+PrintMatrix(matrix);
